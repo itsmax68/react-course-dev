1edff63e751da890aae3f0a740d62e02852c7058
Manav <manavkalathiya828@gmail.com>
2026-06-13 13:14:22 +0530

    generate html for payment summary

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -8,11 +8,17 @@ import './checkoutPage.css'
 export function CheckoutPage({cart}) {
 
     const [deliveryOptions, setDeliveryOption] = useState([]);
+    const [paymentSummary, setpaymentSummary] = useState(null);
 
     useEffect(() => {
         axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
         .then((response) => {
             setDeliveryOption(response.data)
+        });
+
+        axios.get('/api/payment-summary')
+        .then((response) => {
+            setpaymentSummary(response.data);
         });
     }, []);
 
@@ -122,35 +128,41 @@ export function CheckoutPage({cart}) {
                     <div className="payment-summary-title">
                         Payment Summary
                     </div>
+
+                    {paymentSummary && (
+                        <>
+                                <div className="payment-summary-row">
+                                <div>Items ({paymentSummary.totalItems}):</div>
+                                <div className="payment-summary-money">{formatMoney(paymentSummary.productCostCents)}</div>
+                            </div>
+                
+                            <div className="payment-summary-row">
+                                <div>Shipping &amp; handling:</div>
+                                <div className="payment-summary-money">{formatMoney(paymentSummary.shippingCostCents)}</div>
+                            </div>
+                
+                            <div className="payment-summary-row subtotal-row">
+                                <div>Total before tax:</div>
+                                <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostBeforeTaxCents)}</div>
+                            </div>
+                
+                            <div className="payment-summary-row">
+                                <div>Estimated tax (10%):</div>
+                                <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
+                            </div>
+                
+                            <div className="payment-summary-row total-row">
+                                <div>Order total:</div>
+                                <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
+                            </div>
+                
+                            <button className="place-order-button button-primary">
+                                Place your order
+                            </button>
+                    </>
+                    )}
         
-                    <div className="payment-summary-row">
-                        <div>Items (3):</div>
-                        <div className="payment-summary-money">$42.75</div>
-                    </div>
-        
-                    <div className="payment-summary-row">
-                        <div>Shipping &amp; handling:</div>
-                        <div className="payment-summary-money">$4.99</div>
-                    </div>
-        
-                    <div className="payment-summary-row subtotal-row">
-                        <div>Total before tax:</div>
-                        <div className="payment-summary-money">$47.74</div>
-                    </div>
-        
-                    <div className="payment-summary-row">
-                        <div>Estimated tax (10%):</div>
-                        <div className="payment-summary-money">$4.77</div>
-                    </div>
-        
-                    <div className="payment-summary-row total-row">
-                        <div>Order total:</div>
-                        <div className="payment-summary-money">$52.51</div>
-                    </div>
-        
-                    <button className="place-order-button button-primary">
-                        Place your order
-                    </button>
+                    
                 </div>
                 </div>
             </div>
