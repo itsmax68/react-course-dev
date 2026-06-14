509b11551868c1b9c72455bce2314023160495ae
Manav <manavkalathiya828@gmail.com>
2026-06-14 20:31:19 +0530

    create integration test for product component

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -8,6 +8,6 @@ describe('formateMoney', () => {
     
     it('displays 2 decimals', () => {
         expect(formatMoney(1090)).toBe('$10.90');
-        expect(formatMoney(100)).toBe('$1.00')
+        expect(formatMoney(100)).toBe('$1.00');
     });
 });
