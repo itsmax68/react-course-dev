fe94ffc7f9c856961888c74c01d3decc48fda8b5
Manav <manavkalathiya828@gmail.com>
2026-06-14 13:57:25 +0530

    Create delete cart item feature

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import axios from 'axios'
 import {formatMoney} from '../../utils/money';
 import {DeliveryOptions} from './DeliveryOptions'
 
@@ -9,6 +10,16 @@ export function OrderSummary({cart , deliveryOptions ,loadCart}) {
             const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
                 return deliveryOption.id === cartItem.deliveryOptionId;
             });
+
+            const deleteCartItem= async () => {
+                await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                await loadCart();
+            };
+
+            const updateCartItem= async () => {
+                await axios.put(`/api/cart-items/${cartItem.productId}`);
+                await loadCart();
+            };
 
             return(
                 <div key={cartItem.productId} className="cart-item-container">
@@ -31,10 +42,10 @@ export function OrderSummary({cart , deliveryOptions ,loadCart}) {
                 <span>
                 Quantity: <span className="quantity-label">{cartItem.quantity}</span>
                 </span>
-                <span className="update-quantity-link link-primary">
+                <span className="update-quantity-link link-primary" onClick={updateCartItem}>
                 Update
                 </span>
-                <span className="delete-quantity-link link-primary">
+                <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
                 Delete
                 </span>
             </div>
